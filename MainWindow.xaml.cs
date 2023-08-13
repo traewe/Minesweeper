5668f2575af77d971e54c2173a2785898a941072
Oleksii Kovalenko <114068030+traewe@users.noreply.github.com>
2023-08-13 15:16:19 +0300

    Game is ready

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Timers;
+using System.Windows.Threading;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+using Application = System.Net.Mime.MediaTypeNames.Application;
+using System.Security.Policy;
+
+namespace WPF
+{
+    public partial class MainWindow : Window
+    {
+        int height;
+        protected Grid gridWithButtons;
+        StackPanel stackPanelForInfo;
+        protected static bool[,] isClosedCeilsArray;
+        protected static int quantityOfFlags = 0;
+        protected static TextBlock bombsCountTextBlock;
+        TextBlock timerTextBlock;
+        protected static bool isGameOver = false;
+
+        protected static DispatcherTimer timer;
+        int seconds;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            timerTextBlock.Text = (seconds / 3600) + ":" + (seconds / 60) + ":" + (seconds % 60) + " ";
+        }
+        public void escButton_Click(object sender, RoutedEventArgs c) => this.Close();
+        private void CreateFrame(int height)
+        {
+            isClosedCeilsArray = new bool[height, height];
+
+            DockPanel dockPanel = new DockPanel();
+            dockPanel.LastChildFill = true;
+
+            stackPanelForInfo = new StackPanel();
+            stackPanelForInfo.Orientation = Orientation.Horizontal;
+            stackPanelForInfo.HorizontalAlignment = HorizontalAlignment.Center;
+            stackPanelForInfo.VerticalAlignment = VerticalAlignment.Center;
+            stackPanelForInfo.MinHeight = 30;
+            stackPanelForInfo.MaxHeight = 30;
+
+            gridWithButtons = new Grid();
+
+            for (int i = 0; i < height; i++)
+            {
+                RowDefinition rowDef = new RowDefinition();
+                gridWithButtons.RowDefinitions.Add(rowDef);
+            }
+
+            for (int j = 0; j < height; j++)
+            {
+                ColumnDefinition colDef = new ColumnDefinition();
+                gridWithButtons.ColumnDefinitions.Add(colDef);
+            }
+
+            DockPanel.SetDock(stackPanelForInfo, Dock.Top);
+            dockPanel.Children.Add(stackPanelForInfo);
+
+            dockPanel.Children.Add(gridWithButtons);
+
+            Content = dockPanel;
+        }
+        private void CreateField()
+        {
+            CreateFrame(height);
+
+            timerTextBlock = new TextBlock();
+            timerTextBlock.Text = "0:0:0 ";
+            timerTextBlock.FontSize = 27;
+            timerTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+            timerTextBlock.VerticalAlignment = VerticalAlignment.Center;
+            timerTextBlock.MinWidth = 15;
+
+            bombsCountTextBlock = new TextBlock();
+            bombsCountTextBlock.Text = "0";
+            bombsCountTextBlock.FontSize = 27;
+            bombsCountTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+            bombsCountTextBlock.VerticalAlignment = VerticalAlignment.Center;
+            bombsCountTextBlock.MinWidth = 15;
+
+            System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+            image.Source = new BitmapImage(new Uri("C:\\Users\\alex5\\source\\repos\\WPF\\WPF\\images\\ukrainian_flag_white_background.png"));
+            image.MinHeight = 30;
+
+            stackPanelForInfo.Children.Add(timerTextBlock);
+            stackPanelForInfo.Children.Add(bombsCountTextBlock);
+            stackPanelForInfo.Children.Add(image);
+
+            Button buttonEsc = new Button();
+            buttonEsc.Click += (sender, e) => escButton_Click(sender, e);
+            buttonEsc.IsCancel = true;
+            buttonEsc.Width = 0;
+            buttonEsc.Height = 0;
+
+            for (int i = 0; i < gridWithButtons.RowDefinitions.Count; i++)
+            {
+                for (int j = 0; j < gridWithButtons.ColumnDefinitions.Count; j++)
+                {
+                    int iCopy = i;
+                    int jCopy = j;
+
+                    Button button = new Button();
+                    button.Background = new SolidColorBrush(Color.FromRgb(112, 225, 255));
+
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+                    gridWithButtons.Children.Add(button);
+                }
+            }
+            for (int i = 0; i < gridWithButtons.RowDefinitions.Count; i++)
+            {
+                for (int j = 0; j < gridWithButtons.ColumnDefinitions.Count; j++)
+                {
+                    int iCopy = i;
+                    int jCopy = j;
+
+                    ((Button)gridWithButtons.Children[i * height + j]).Click += (sender, e) => MineSweeper.TransmitDataAboutFirstClick(iCopy, jCopy);
+                    ((Button)gridWithButtons.Children[i * height + j]).Click += (sender, e) => MineSweeper.GenerateField(height);
+                    ((Button)gridWithButtons.Children[i * height + j]).Click += (sender, e) => MineSweeper.OpenCeil(iCopy, jCopy, gridWithButtons.Children, true, false);
+                    ((Button)gridWithButtons.Children[i * height + j]).Click += (sender, e) => UpdateDataAboutQuantityOfFlags();
+
+                    ((Button)gridWithButtons.Children[i * height + j]).MouseRightButtonDown += (sender, e) => MakeFlag(iCopy, jCopy, gridWithButtons.Children);
+                    ((Button)gridWithButtons.Children[i * height + j]).MouseRightButtonDown += (sender, e) => UpdateDataAboutQuantityOfFlags();
+                }
+            }
+
+            gridWithButtons.Children.Add(buttonEsc);
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+
+            seconds = 0;
+
+            timer.Start();
+        }
+        private void UpdateDataAboutQuantityOfFlags()
+        {
+            if (!isGameOver)
+                bombsCountTextBlock.Text = (MineSweeper.QuantityOfBombs() - quantityOfFlags).ToString();
+        }
+        private void DataApprovalButtonClick(object sender, RoutedEventArgs e)
+        {
+            string inputDataStr = textBoxHeight.Text;
+            int givenData;
+
+            if (!int.TryParse(inputDataStr, out givenData) || Convert.ToInt32(inputDataStr) < 5 || Convert.ToInt32(inputDataStr) > 50)
+                MessageBox.Show("Будь ласка, введіть ціле число від 5 до 50 включно");
+            else
+            {
+                height = givenData;
+
+                CreateField();
+            }
+        }
+        private void MakeFlag(int i, int j, UIElementCollection children)
+        {
+            System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+            image.Source = new BitmapImage(new Uri("C:\\Users\\alex5\\source\\repos\\WPF\\WPF\\images\\ukrainian_flag.png"));
+
+            if (((SolidColorBrush)((Button)children[i * height + j]).Background).Color != Color.FromRgb(112, 225, 255) || isGameOver)
+                return;
+
+            if (isClosedCeilsArray[i, j] == false)
+            {
+                ((Button)children[i * height + j]).Content = image;
+                isClosedCeilsArray[i, j] = true;
+                quantityOfFlags++;
+            }
+            else
+            {
+                ((Button)children[i * height + j]).Content = null;
+                isClosedCeilsArray[i, j] = false;
+                quantityOfFlags--;
+            }
+        }
+    }
+}
---
+++ MineSweeper.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+using System.Threading;
+
+namespace WPF
+{
+    public class MineSweeper : MainWindow
+    {
+        static int height;
+        static int[,] field;
+        static bool[,] visitedCeils;
+        static int firstClickHeight = -1;
+        static int firstClickWidth = -1;
+        static int quantityOfBombs = 0;
+        static int quantityOfOpenedCeils = 0;
+        
+        public static void GenerateField(int givenHeight)
+        {
+            if (field != null)
+                return;
+
+            height = givenHeight;
+            field = new int[height, height];
+            visitedCeils = new bool[height, height];
+
+            Random rnd = new Random();
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (!(i == firstClickHeight && j == firstClickWidth) && rnd.Next(1, 8) == 3)
+                    {
+                        field[i, j] = 10;
+                        quantityOfBombs++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (field[i, j] != 10)
+                        field[i, j] = CountMines(i, j);
+                }
+            }
+        }
+        public static void TransmitDataAboutFirstClick(int height, int width)
+        {
+            if (firstClickHeight == -1 && firstClickWidth == -1)
+            {
+                firstClickHeight = height;
+                firstClickWidth = width;
+            }
+        }
+        public static int QuantityOfBombs() => quantityOfBombs;
+        static int CountMines(int i, int j)
+        {
+            int counter = 0;
+            int[] firstCoordinates = { i - 1, i, i + 1 };
+            int[] secondCoordinates = { j - 1, j, j + 1 };
+
+            foreach (int firstCoordinate in firstCoordinates)
+            {
+                foreach (int secondCoordinate in secondCoordinates)
+                {
+                    if (firstCoordinate < 0 || firstCoordinate > height - 1 || secondCoordinate < 0 || secondCoordinate > height - 1)
+                        continue;
+                    else if (field[firstCoordinate, secondCoordinate] == 10)
+                        counter++;
+                }
+            }
+            return counter;
+        }
+        public static void OpenCeil(int i, int j, UIElementCollection children, bool playerClicked = false, bool reversion = true)
+        {
+            if (i < 0 || i > height - 1 || j < 0 || j > height - 1 || visitedCeils[i, j] || (isClosedCeilsArray[i, j] && reversion == false) || isGameOver)
+                return;
+
+            visitedCeils[i, j] = true;
+
+            if (field[i, j] == 10 && playerClicked)
+            {
+                for (int n = 0; n < height; n++)
+                {
+                    for (int m = 0; m < height; m++)
+                    {
+                        if (field[n, m] == 10)
+                        {
+                            System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+                            image.Source = new BitmapImage(new Uri("C:\\Users\\alex5\\source\\repos\\WPF\\WPF\\images\\bomb.png"));
+                            ((Button)children[n * height + m]).Content = image;
+                        }
+                    }
+                }
+                bombsCountTextBlock.Text = "Поразка :( Спробуй ще раз і у тебе вийде";
+                isGameOver = true;
+                timer.Stop();
+            }
+
+            else if (field[i, j] != 10)
+            {
+                quantityOfOpenedCeils++;
+
+                if (isClosedCeilsArray[i, j])
+                    quantityOfFlags--;
+
+                ((Button)children[i * height + j]).Background = new SolidColorBrush(Colors.White);
+                
+                if (field[i, j] == 0)
+                {
+                    ((Button)children[i * height + j]).Content = null;
+                    OpenCeil(i - 1, j, children);
+                    OpenCeil(i + 1, j, children);
+                    OpenCeil(i, j - 1, children);
+                    OpenCeil(i, j + 1, children);
+                    OpenCeil(i - 1, j - 1, children);
+                    OpenCeil(i + 1, j - 1, children);
+                    OpenCeil(i - 1, j + 1, children);
+                    OpenCeil(i + 1, j + 1, children);
+                }
+                else
+                {
+                    System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+                    image.Source = new BitmapImage(new Uri("C:\\Users\\alex5\\source\\repos\\WPF\\WPF\\images\\" + Convert.ToString(field[i, j]) + ".png"));
+                    ((Button)children[i * height + j]).Content = image;
+
+                    if (i != 0 && field[i - 1, j] == 0)
+                        OpenCeil(i - 1, j, children);
+                    if (i != height - 1 && field[i + 1, j] == 0)
+                        OpenCeil(i + 1, j, children);
+                    if (j != 0 && field[i, j - 1] == 0)
+                        OpenCeil(i, j - 1, children);
+                    if (j != height - 1 && field[i, j + 1] == 0)
+                        OpenCeil(i, j + 1, children);
+                }
+            }
+            visitedCeils[i, j] = true;
+
+            if (quantityOfOpenedCeils == height * height - quantityOfBombs)
+            {
+                bombsCountTextBlock.Text = "Перемога! Пес патрон тобою пишається!";
+                isGameOver = true;
+                timer.Stop();
+                for (int n = 0; n < height; n++)
+                {
+                    for (int m = 0; m < height; m++)
+                    {
+                        if (field[n, m] == 10)
+                        {
+                            System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+                            image.Source = new BitmapImage(new Uri("C:\\Users\\alex5\\source\\repos\\WPF\\WPF\\images\\bomb.png"));
+                            ((Button)children[n * height + m]).Content = image;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
